c42e79e45381fd6062e71fad08a0d1504d102af5
Carole <cjpillette@hotmail.com>
2017-05-09 21:55:11 +1000

    styled views for index, skeins and yarn

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
   resources :searches
   put '/pages/cart', to: 'pages#cart', as: 'cart'
   get '/pages/contact', to: 'pages#contact', as: 'contact'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
