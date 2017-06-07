95367f9d0b42b33efce3116826debad853a71b99
Carole <cjpillette@hotmail.com>
2017-06-07 13:59:30 +1000

    Add to cart button now adds to cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -26,13 +26,19 @@ class LineItemsController < ApplicationController
   def create
     skein = Skein.find(params[:skein_id])
     @line_item = @cart.line_items.build(skein: skein)
-      respond_to do |format| if @line_item.save
-          redirect_to @line_item, notice: 'Line item was successfully created.'
-          else
-            render :new
-          end
+    respond_to do |format|
+      if @line_item.save
+        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+        format.json { render :show,
+          status: :created, location: @line_item }
+      else
+          format.html { render :new }
+          format.json { render json: @line_item.errors,
+            status: :unprocessable_entity }
       end
-end
+    end
+  end
+
 
   # PATCH/PUT /line_items/1
   def update
